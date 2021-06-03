2697d5a49177e2a7734528b9f15e12c28511f5c0
Ian R Knight <ianknight7000@gmail.com>
2021-06-02 19:13:30 -0700

    updates classes, provides container bones

--- lambdata/helper_functions.py
+++ lambdata/helper_functions.py
@@ -1,5 +1,13 @@
 import pandas as pd
+from faker import Faker
+
+fake = Faker()
+
 df = pd.read_csv("https://raw.githubusercontent.com/iknight7000/lambdata-knight/main/Hospitals.csv")
+
+new_addresses = []
+for _ in range(10):
+    new_addresses.append(fake.address())
 
 class Null_Count:
     """A Null Count class with methods and attributes"""
@@ -9,12 +17,23 @@ class Null_Count:
     # df is the argument that has to be passed in to the object
     def __init__(self,df):
         """This is a constructor method"""
+        self.df = df
         # attributes
         #This is the creation of the attribute
         #We are defining the attribute here
         
 
-    def clean_data(df):
+    def clean_data(self):
         return df.isnull().sum().sum()
 
-Null_Count.clean_data(df)
+
+class Add_Series:
+    def __init__(self,df):
+        self.df = df 
+
+    def list_to_series(list,df):
+        df[""] = pd.Series(list)
+
+Add_Series.list_to_series(new_addresses,df)
+print(Null_Count.clean_data(df))
+print(df.columns)
--- lambdata/test_functions.py
+++ lambdata/test_functions.py
@@ -1,5 +1,13 @@
 import pandas as pd
+from faker import Faker
+
+fake = Faker()
+
 df = pd.read_csv("https://raw.githubusercontent.com/iknight7000/lambdata-knight/main/Hospitals.csv")
+
+new_addresses = []
+for _ in range(10):
+    new_addresses.append(fake.address())
 
 class Null_Count:
     """A Null Count class with methods and attributes"""
@@ -9,6 +17,7 @@ class Null_Count:
     # df is the argument that has to be passed in to the object
     def __init__(self,df):
         """This is a constructor method"""
+        self.df = df
         # attributes
         #This is the creation of the attribute
         #We are defining the attribute here
@@ -17,4 +26,12 @@ class Null_Count:
     def clean_data(df):
         return df.isnull().sum().sum()
 
-print(Null_Count.clean_data(df))
+
+
+class Add_Series:
+    def __init__(self,df):
+        self.df = df 
+
+    def list_to_series(list,df):
+        df[""] = pd.Series(list)
+
